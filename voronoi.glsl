e50c7599e89e2e027fe90169e5c66a2771f96223
darius.becher@wandelbots.com <Darius.Becher@wandelbots.com>
2023-11-03 18:39:53 +0100

    added points to array

--- voronoi.glsl
+++ voronoi.glsl
@@ -25,12 +25,14 @@ vec2 getPointAtCursor(vec2 pos, vec2 mouse) {
 void main() {
 
     vec2 mouse = u_mouse.xy / u_resolution;
-    float light = 1.0;
+    vec3 light = vec3(1.);
     float cellcore_radius = 0.01;
 
     vec3 col = vec3(0);
-    int cell_count = 15;
+    const int cell_count = 15;
+    vec2 array[cell_count];
     vec3 cells = vec3(1);
+
 
 
     for(int i=0; i<cell_count; i++) {
@@ -40,20 +42,36 @@ void main() {
             random(vec2(i+1)*0.5*cos(float(i)*.2387))
         );
 
+        
+
         pos = getPointAtCursor(pos, mouse); //Setzt (bei gehaltener Maus) Voronoi-Zelle an Cursor 
         
 
         pos.x = min(pos.x, 1.);
         pos.y = min(pos.y, 1.);
         
+        array[i] = pos;
+
         float dist = length(pos-uv);
-        light *= step(cellcore_radius, dist);
-        float cell_width = abs(sin(u_time))*.1;
+        if(dist <= cellcore_radius) {
+            light *= vec3(1,0,0);
+        }
+        float cell_width = .11;
         
         if((dist) <= cell_width) {
             //cells = vec3(dist+random(pos*sin(.122736378)), random(pos*sin(0.232736)), random(pos*sin(.2328173)));
-            cells *= vec3(1.-dist*5.);
+           cells *= vec3(1.-dist*5.);
+        }
+
+    }
+
+    float diff = 0.;
+
+    for(int i=0; i<cell_count; i++) {
+        for(int j=0; j<cell_count; j++) {
+            diff = length((array[i]-uv)-(array[j]-uv)) / 2.;
         }
     }
+
     out_color = vec4(col + light * cells, 1.0);
 }
