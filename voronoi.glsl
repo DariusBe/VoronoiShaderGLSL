9863a08f4b42582016111532c80b99d77232e334
TobiasAdler <tobias.adler2@stud.htw-dresden.de>
2023-11-04 16:42:02 +0100

    Implemented new voronoi generation

--- voronoi.glsl
+++ voronoi.glsl
@@ -6,72 +6,50 @@ precision highp sampler2D;
 in vec2 uv;
 out vec4 out_color;
 
-uniform vec2 u_resolution;
-uniform float u_time;
-uniform vec4 u_mouse;
+
+// Generiert für übergebene number jeweils den selben Farbwert
+vec3 randColor(int number){
+    return fract(sin(vec3(number+1)*vec3(12.8787, 19.97, 20.73739)));
+}
 
 float random(vec2 vec) {
     return fract(sin(dot(vec.xy, vec2(233.33322, 122.233382)))*41422.232333);
 }
 
-vec2 getPointAtCursor(vec2 pos, vec2 mouse) {
-        if(u_mouse.p == 1.) { 
-            pos.x = vec2(mouse).x; 
-            pos.y = 1.-vec2(mouse).y; 
-        }
-        return pos;
-}
-
 void main() {
 
-    vec2 mouse = u_mouse.xy / u_resolution;
-    vec3 light = vec3(1.);
+    vec3 col = vec3(1); 
     float cellcore_radius = 0.01;
-
-    vec3 col = vec3(0);
-    const int cell_count = 15;
-    vec2 array[cell_count];
-    vec3 cells = vec3(1);
+    
+    const int points_length = 25; 
+    vec2 points_array[points_length];
 
 
-
-    for(int i=0; i<cell_count; i++) {
+    
+    float dmin = 1000.0; 
+    int point = 0;
+    
+    for(int i = 0; i < points_length; i++){
 
         vec2 pos = vec2(
-            random(vec2(i+1)),
-            random(vec2(i+1)*0.5*cos(float(i)*.2387))
+          random(vec2(i+1)),
+          random(vec2(i+1)*0.5*cos(float(i)*.2387))
         );
 
-        
+        points_array[i] = pos;
 
-        pos = getPointAtCursor(pos, mouse); //Setzt (bei gehaltener Maus) Voronoi-Zelle an Cursor 
+        vec2 uv_rounded = floor(uv*100.0)/100.0;
+        vec2 point_rounded = floor(points_array[i]*100.0)/100.0;
         
-
-        pos.x = min(pos.x, 1.);
-        pos.y = min(pos.y, 1.);
-        
-        array[i] = pos;
-
-        float dist = length(pos-uv);
-        if(dist <= cellcore_radius) {
-            light *= vec3(1,0,0);
+        if(length(uv-pos) <= cellcore_radius){
+            col = vec3(0); 
         }
-        float cell_width = .11;
-        
-        if((dist) <= cell_width) {
-            //cells = vec3(dist+random(pos*sin(.122736378)), random(pos*sin(0.232736)), random(pos*sin(.2328173)));
-           cells *= vec3(1.-dist*5.);
-        }
-
-    }
-
-    float diff = 0.;
-
-    for(int i=0; i<cell_count; i++) {
-        for(int j=0; j<cell_count; j++) {
-            diff = length((array[i]-uv)-(array[j]-uv)) / 2.;
+        if(length(points_array[i] - uv) < dmin){
+            point = i; 
+            dmin = length(points_array[i] - uv); 
         }
     }
 
-    out_color = vec4(col + light * cells, 1.0);
+    // Output to screen
+    out_color = vec4((randColor(point)-dmin)*col,1.0);
 }
